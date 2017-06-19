ec93a59569368075bd85d3a1ba28cd7c33f5a0cb
Harry Lucas <harry@lucasconsulting.com.au>
2017-06-19 13:54:57 +1000

    added ability to time

---
+++ lib/util.ex
@@ -0,0 +1,22 @@
+defmodule Util do
+
+  @doc """
+  Records the time taken to execute the passed in function.
+
+  Optionally takes in a `times` argument which will run the function
+  that number of times and return the average result.
+
+  ```elixir
+  iex> Util.time(fn() -> 1 + 1 end, 100)
+  5.32
+  ```
+  """
+  def time(func, times \\ 1) do
+    for _ <- 1..times do
+      :timer.tc(fn -> func.() end)
+      |> elem(0)
+    end
+    |> Enum.sum
+    |> Kernel./(times)
+  end
+end
